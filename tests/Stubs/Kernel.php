563fc341477be64a37fa96d2ffdd1b79c4dd60ed
Ivan Vermeyen <ivan@vermeyen.org>
2023-08-18 12:35:53 +0200

    Create Kernel.php

---
+++ tests/Stubs/Kernel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace CodeZero\Localizer\Tests\Stubs;
+
+use Illuminate\Foundation\Http\Kernel as HttpKernel;
+
+class Kernel extends HttpKernel
+{
+    /**
+     * The application's route middleware groups.
+     *
+     * @var array
+     */
+    protected $middlewareGroups = [
+        'web' => [
+            \Illuminate\Cookie\Middleware\EncryptCookies::class,
+            \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
+            \Illuminate\Session\Middleware\StartSession::class,
+            // \Illuminate\Session\Middleware\AuthenticateSession::class,
+            \Illuminate\View\Middleware\ShareErrorsFromSession::class,
+            \CodeZero\Localizer\Middleware\SetLocale::class, // <== Added Middleware Here
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
+        ],
+    ];
+}
